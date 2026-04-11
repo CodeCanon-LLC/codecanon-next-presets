7a35e7f83e08082bba090cd2364b1014b83e1404
Abdelrahman Salem <abubakir1997@gmail.com>
2026-04-11 14:00:38 -0400

    feat: update playground favicon and title

--- playground/src/index.tsx
+++ playground/src/index.tsx
@@ -1,13 +1,10 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import { App } from "./App.tsx"
-import { PresetProvider } from "@codecanon/next-presets"
 import "./style.css"
 
 createRoot(document.querySelector("#app")!).render(
   <StrictMode>
-    <PresetProvider defaultTheme="dark">
-      <App />
-    </PresetProvider>
+    <App />
   </StrictMode>
 )
